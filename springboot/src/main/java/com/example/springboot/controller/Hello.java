29bd401d1ba992253f8924bb1c89cad5e4783ab1
wukn2018 <18325713417@163.com>
2018-11-15 17:20:40 +0800

    测试2

--- springboot/src/main/java/com/example/springboot/controller/Hello.java
+++ springboot/src/main/java/com/example/springboot/controller/Hello.java
@@ -1,5 +1,7 @@
 package com.example.springboot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,9 +15,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Component
 public class Hello {
 
+    Logger logger = LoggerFactory.getLogger(Hello.class);
+
     @RequestMapping("hellow")
     @ResponseBody
     public String  helloWord(){
+        logger.info("ss");
         return "hello word";
     }
 
